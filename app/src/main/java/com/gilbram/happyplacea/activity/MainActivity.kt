dc37d3eb4400ba8e7be63d251c3320d8400f539e
GilbramAlifMiftahulFuan <fuanguns@gmail.com>
2020-11-09 13:31:37 +0700

    Initial commit

---
+++ app/src/main/java/com/gilbram/happyplacea/activity/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.gilbram.happyplacea.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.gilbram.happyplacea.R
+import com.gilbram.happyplacea.adapter.HappyPlacesAdapter
+import com.gilbram.happyplacea.database.DatabaseHandler
+import com.gilbram.happyplacea.model.HappyPlaceModel
+import com.gilbram.happyplacea.util.SwipeToDeleteCallback
+import com.gilbram.happyplacea.util.SwipeToEditCallback
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.ArrayList
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        var ADD_PLACE_ACTIVITY_REQUEST_CODE = 1
+        var EXTRA_PLACE_DETAIL = "extra_place_detail"
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fab_add.setOnClickListener {
+            val intent = Intent(this, AddHappyPlaceActivity::class.java)
+            startActivityForResult(intent, ADD_PLACE_ACTIVITY_REQUEST_CODE)
+        }
+        getHappyPlacesListFromLocalDB()
+    }
+
+    private fun getHappyPlacesListFromLocalDB() {
+        val dbHandler = DatabaseHandler(this)
+        val getHappyPlacesList = dbHandler.getHappyPlaceList()
+        if (getHappyPlacesList.size > 0) {
+            rv_happy_places_list.visibility = View.VISIBLE
+            tv_no_records.visibility = View.GONE
+            setupHappyPlacesRecyclerView(getHappyPlacesList)
+        } else {
+            rv_happy_places_list.visibility = View.GONE
+            tv_no_records.visibility = View.VISIBLE
+        }
+    }
+
+    private fun setupHappyPlacesRecyclerView(happyPlacesList: ArrayList<HappyPlaceModel>) {
+        rv_happy_places_list.layoutManager = LinearLayoutManager(this)
+        rv_happy_places_list.setHasFixedSize(true)
+
+        val placesAdapter = HappyPlacesAdapter(this, happyPlacesList)
+        rv_happy_places_list.adapter = placesAdapter
+
+        placesAdapter.setOnClickListener(object :
+            HappyPlacesAdapter.OnClickListener {
+            override fun onClick(position: Int, model: HappyPlaceModel) {
+                val intent = Intent(this@MainActivity, HappyDetailActivity::class.java)
+                intent.putExtra(EXTRA_PLACE_DETAIL, model)
+                startActivity(intent)
+            }
+        })
+        val editSwipeHandler = object : SwipeToEditCallback(this){
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val adapter = rv_happy_places_list.adapter as HappyPlacesAdapter
+                adapter.notifyEditItem(
+                        this@MainActivity,viewHolder.adapterPosition, ADD_PLACE_ACTIVITY_REQUEST_CODE
+                )
+            }
+        }
+        val editItemTouchHelper = ItemTouchHelper(editSwipeHandler)
+        editItemTouchHelper.attachToRecyclerView(rv_happy_places_list)
+
+        val deleteSwipeHandler = object : SwipeToDeleteCallback(this){
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val adapter = rv_happy_places_list.adapter as HappyPlacesAdapter
+                adapter.removeAt(viewHolder.adapterPosition)
+                getHappyPlacesListFromLocalDB()
+            }
+        }
+        val deleteItemTouchHelper = ItemTouchHelper(deleteSwipeHandler)
+        deleteItemTouchHelper.attachToRecyclerView(rv_happy_places_list)
+    }
+}
